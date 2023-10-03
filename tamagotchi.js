20f0fb7a2958e0e11eafb27cdfe10100476207c0
mherrera491 <mattherrera1@gmail.com>
2023-10-02 18:42:30 -0700

    added age up function

--- tamagotchi.js
+++ tamagotchi.js
@@ -12,6 +12,7 @@ class VirtualPet {
       this.hunger += 10;
     } else {
       this.hunger = 100;
+      alert(`${this.name} is full!`)
     }
   }
 
@@ -20,6 +21,7 @@ class VirtualPet {
       this.energy += 10;
     } else {
       this.energy = 100;
+      alert(`${this.name} is not sleepy!`)
     }
   }
 
@@ -28,23 +30,49 @@ class VirtualPet {
       this.fun += 10;
     } else {
       this.fun = 100;
-      console.log(`${this.name} has had enough playtime.`);
+      alert(`${this.name} has had enough playtime.`);
     }
   }
+
+  increaseAge() {
+    this.age +=1;
+    const ageDisplay = document.getElementById("age");
+    ageDisplay.textContent = this.age;
+}
 }
 
-const petName = prompt("What is your pet's name?");
+// Creating the prompt for the name
+const petName = prompt("What is your virtual pet's name?");
 
+// creating a new instance of the VirtualPet class using the name given from the prompt
 const myPet = new VirtualPet(petName);
 
+// console.log(myPet)
+
 let newName = document.getElementById("pet-name")
-newName.innerHTML = petName
+newName.innerHTML = `${petName} says hi!`
+
+// Created function that calls the increaseAge() function after a certain amount of time
+function ageIncreaseTimer() {
+    setInterval(function () {
+        myPet.increaseAge()
+    }, 3000);
+}
+ageIncreaseTimer();
+
+const eatButton = document.querySelector("#feed-button");
+const sleepButton = document.querySelector("#sleep-button");
+const playButton = document.querySelector("#play-button");
+
+eatButton.addEventListener("click", function() {
+    myPet.eatFood();
+})
+sleepButton.addEventListener("click", function() {
+    myPet.sleep();
+})
+playButton.addEventListener("click", function() {
+    myPet.playTime();
+})
 
 
-
-
-
-const eatButton = document.querySelector(".feed-button");
-const sleepButton = document.querySelector(".sleep-button");
-const playButton = document.querySelector(".play-button");
 
